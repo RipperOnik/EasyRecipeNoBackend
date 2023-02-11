9029c40735d693a536e127acee5fe386913a1822
RipperOnik <kingstrong2@mail.ru>
2023-02-11 22:51:47 +0300

    Updates

--- components/subcomponents/specialCases/LoadingSpinner.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var react_1 = require("react");
-function LoadingSpinner() {
-    return (react_1.default.createElement("div", { className: "spinner-border text-primary", role: "status" },
-        react_1.default.createElement("span", { className: "visually-hidden" }, "Loading...")));
-}
-exports.default = LoadingSpinner;
-//# sourceMappingURL=LoadingSpinner.js.map
